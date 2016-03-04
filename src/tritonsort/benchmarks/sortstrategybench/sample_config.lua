fa489a80a83ca94a8460ff587b941ae331bf1e91
Mike Conley <mconley@google.com>
2016-03-04 12:02:44 -0600

    Remove gsl dependency, and therefore click log generator, vargensort, and sort strategy bench

--- src/tritonsort/benchmarks/sortstrategybench/sample_config.lua
+++
@@ -1,9 +0,0 @@
-dofile("PATH/src/tritonsort/mapreduce/config.default.mapreduce")
-
-MEM_SIZE = 25769803776
-ALLOCATOR_CAPACITY = math.floor(0.90 * MEM_SIZE)
-
-CORES_PER_NODE = 16
-THREAD_CPU_POLICY="CUSTOM"
-THREAD_POLICY_reader              = "0000000100000000"
-THREAD_POLICY_writer              = "0000000000000001"
