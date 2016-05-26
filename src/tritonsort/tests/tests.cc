45e5ee4c2717ed1964bf1936d70863be50b0da1e
Mike Conley <mconley@google.com>
2016-05-26 16:48:22 -0500

    remove cppunit

--- src/tritonsort/tests/tests.cc
+++ src/tritonsort/tests/tests.cc
@@ -1,15 +1,10 @@
 #include <boost/filesystem.hpp>
-#include <cppunit/TestResult.h>
-#include <cppunit/TestResultCollector.h>
-#include <cppunit/TestRunner.h>
-#include <cppunit/TextOutputter.h>
-#include <cppunit/TextTestProgressListener.h>
-#include <cppunit/XmlOutputter.h>
 #include <iostream>
 #include <limits>
 #include <signal.h>
 #include <stdlib.h>
 #include <string>
+#include "gtest/gtest.h"
 
 #include "common/MainUtils.h"
 #include "config.h"
@@ -17,7 +12,6 @@
 #include "core/TritonSortAssert.h"
 #include "core/Utils.h"
 #include "core/constants.h"
-
 
 const char* TEST_WRITE_ROOT;
 const char* TEST_BINARY_ROOT;
@@ -61,35 +55,6 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  CppUnit::TestResult controller;
-  CppUnit::TestResultCollector result;
-  controller.addListener(&result);
-
-  CppUnit::TextTestProgressListener progress;
-  controller.addListener( &progress );
-
-  CppUnit::TestRunner runner;
-
-  std::string testPath;
-
-  runner.run(controller, testPath);
-
-  CppUnit::Outputter* outputter = NULL;
-
-  if (params.contains("DUMP_XML_FILE")) {
-    std::ofstream xmlFileStream(
-      params.get<std::string>("DUMP_XML_FILE").c_str());
-
-    outputter = new CppUnit::XmlOutputter(&result, xmlFileStream);
-    outputter->write();
-
-    xmlFileStream.close();
-  } else {
-    outputter = new CppUnit::TextOutputter(&result, std::cerr);
-    outputter->write();
-  }
-
-  delete outputter;
-
-  return 0;
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
