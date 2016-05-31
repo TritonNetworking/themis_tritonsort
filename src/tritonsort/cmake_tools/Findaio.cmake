6b69837ced9acf00d8a5cb2ebe38c12ef6f83101
Mike Conley <mconley@google.com>
2016-05-31 14:30:37 -0500

    add explicit libaio include directory

--- src/tritonsort/cmake_tools/Findaio.cmake
+++ src/tritonsort/cmake_tools/Findaio.cmake
@@ -1,8 +1,10 @@
+FIND_PATH(LIBAIO_INCLUDE_DIR libaio.h)
+
 FIND_LIBRARY(aio_LIBRARY aio)
 
-MARK_AS_ADVANCED(aio_LIBRARY)
+MARK_AS_ADVANCED(aio_LIBRARY LIBAIO_INCLUDE_DIR)
 
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(aio
-  "aio library is required" aio_LIBRARY)
+  "aio library is required" aio_LIBRARY LIBAIO_INCLUDE_DIR)
